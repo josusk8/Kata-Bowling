8c6a5b912f64affcb1e9bc6707991fc4c7d7bd71
Wolfero <ferorock44@gmail.com>
2022-05-13 10:48:47 +0100

    🔴 have_strike_roll

--- src/test/java/BowlingShould.java
+++ src/test/java/BowlingShould.java
@@ -1,2 +1,32 @@
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class BowlingShould {
+
+    /* Rules
+        We will not check for valid rolls.
+        We will not check for correct number of rolls and frames.
+        We will not provide scores for intermediate frames.
+    */
+
+    /*TODO
+     * ["x"]=> 10
+     * ["9;-"]=> 9
+     * ["5;/"]=> 10
+     * ["-";"5";""]=> 5
+     * ["1";"/";"7"]=> 17
+     * ["X";"X";"9"]=> 29
+     * */
+
+    @Test
+    public void have_strike_roll() {
+        var bowling = new Bowling();
+
+        var results = bowling.rolls(List.of("x"));
+
+        assertThat(results).isEqualTo(10);
+    }
 }
